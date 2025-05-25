215274f7c1d803a4d96db8be35f75c7bc1caaa02
duy59 <vuduy050903@gmail.com>
2025-05-25 21:58:21 +0700

    new

--- app.py
+++ app.py
@@ -288,4 +288,4 @@ def compare_results():
         return jsonify({'error': f'Error comparing results: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=9005)
